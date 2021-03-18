0c806c78658e2b2cade32d70d40b24dfb5fd0cb7
Erico Almeida <erico.silva@invillia.com>
2021-03-18 02:20:10 -0400

    Added feature to mark user's todo as done

--- src/index.js
+++ src/index.js
@@ -98,7 +98,23 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
 });
 
 app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { headers, params } = request;
+  const { username } = headers;
+  const { id } = params;
+
+  const { todos } = users.find((user) => user.username === username);
+
+  const todoExists = todos.some((todo) => todo.id === id);
+  
+  if(!todoExists){
+    return response.status(404).json({error: 'Todo not found!'});
+  }
+
+  const todoIndex = todos.findIndex((todo) => todo.id === id);
+
+  todos[todoIndex].done = true;
+
+  return response.json(todos[todoIndex]); 
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
